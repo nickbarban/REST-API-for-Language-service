7cbcb85e72b1848e547e337e43e725918865be36
Nick Barban <nn.barban@gmail.com>
2016-01-09 00:33:00 +0200

    Refactor

--- src/main/java/com/barban/RestLanguageServiceApplication.java
+++ src/main/java/com/barban/RestLanguageServiceApplication.java
@@ -3,6 +3,7 @@ package com.barban;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
@@ -10,13 +11,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 						// @EnableAutoConfiguration and @ComponentScan with
 						// their default attributes
 //@EnableJpaRepositories // activates Spring Data JPA
-@Import(RepositoryRestMvcConfiguration.class) // mports a collection of Spring
+@Import(RepositoryRestMvcConfiguration.class) // imports a collection of Spring
 												// MVC controllers, JSON
 												// converters, and other beans
 												// needed to provide a RESTful
 												// front end.
 @EnableWebSecurity
-//@PropertySource("application.properties")
+@PropertySource("application.properties")
 public class RestLanguageServiceApplication {
 
 	public static void main(String[] args) {
